3c462dea6433758d5382c8c20b50aedf8381cc11
vassglo <64547397+vassglo@users.noreply.github.com>
2020-05-04 19:49:05 +0200

    add jquery

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
-console.log ('Script file is loaded.')
+$('h1').html('This is done by <em>JavaScript</em>!');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '10px');
+console.log ('Script file is loaded.');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+$('h2').toggleClass('blue');
+// single comment
+/* több soros 
+komment is
+lehet ameddig le nem zárom */
